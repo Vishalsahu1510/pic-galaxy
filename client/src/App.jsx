e3a8e65cdc922d65b125024e853381780a4665c3
Vishal Sahu <vishalsahu0644@gmail.com>
2024-06-30 19:03:56 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import {BrowserRouter , Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import SellerDashboard from "./pages/SellerDashboard";
+import BuyerDashboard from "./pages/BuyerDashboard";
+import Navbar from "./components/Navbar";
+
+
+
+function App() {
+    return (
+    <>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/buyer/profile" element={<BuyerDashboard />} />
+          <Route path="/seller/profile" element={<SellerDashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
